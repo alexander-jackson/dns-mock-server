23056d1bcf84d360129614868bd1993767f8a801
Alexander Jackson <alexanderjackson@protonmail.com>
2023-10-15 12:31:12 +0100

    feat: get test passing
    This is largely copied from a blog post online but will get the test
    passing. Essentially all we do is return the requester's IP address for
    every incoming request.
    
    This change:
    * Adds the basics of the server handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,16 @@
+use std::net::IpAddr;
+
 use async_trait::async_trait;
 use tokio::net::UdpSocket;
+use trust_dns_server::proto::{
+    op::Header,
+    rr::{
+        rdata::{A, AAAA},
+        RData, Record,
+    },
+};
 use trust_dns_server::{
+    authority::MessageResponseBuilder,
     server::{Request, RequestHandler, ResponseHandler, ResponseInfo},
     ServerFuture,
 };
@@ -24,9 +34,22 @@ impl Server {
 impl RequestHandler for Server {
     async fn handle_request<R: ResponseHandler>(
         &self,
-        _request: &Request,
-        _response: R,
+        request: &Request,
+        mut response_handler: R,
     ) -> ResponseInfo {
-        todo!()
+        let builder = MessageResponseBuilder::from_message_request(request);
+
+        let mut header = Header::response_from_request(request.header());
+        header.set_authoritative(true);
+
+        let rdata = match request.src().ip() {
+            IpAddr::V4(ipv4) => RData::A(A::from(ipv4)),
+            IpAddr::V6(ipv6) => RData::AAAA(AAAA::from(ipv6)),
+        };
+
+        let records = vec![Record::from_rdata(request.query().name().into(), 60, rdata)];
+        let response = builder.build(header, records.iter(), &[], &[], &[]);
+
+        response_handler.send_response(response).await.unwrap()
     }
 }
