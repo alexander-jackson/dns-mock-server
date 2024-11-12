7d94d9f24eb6722e699339ae73a2a88e5ac72bd4
Denis Davydov <ddavydov@cloudflare.com>
2024-11-12 21:54:24 +0000

    Add a method to mock responses with error codes

--- tests/mod.rs
+++ tests/mod.rs
@@ -6,7 +6,7 @@ use hickory_resolver::proto::op::ResponseCode;
 use hickory_resolver::AsyncResolver;
 use tokio::net::UdpSocket;
 
-use dns_mock_server::Server;
+use dns_mock_server::{Response, Server};
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
@@ -69,6 +69,37 @@ async fn unknown_names_return_errors() -> Result<()> {
     };
 
     assert_eq!(*response_code, ResponseCode::ServFail);
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn can_query_desired_response_from_the_server() -> Result<()> {
+    let mut server = Server::default();
+    server.add_response("www.example.com.", Response::Code(ResponseCode::NXDomain))?;
+
+    let addr = SocketAddrV4::new(Ipv4Addr::LOCALHOST, 0);
+    let socket = UdpSocket::bind(&addr).await?;
+
+    let local_addr = socket.local_addr()?;
+
+    tokio::spawn(async move {
+        server.start(socket).await.unwrap();
+    });
+
+    let mut config = ResolverConfig::new();
+    let nameserver_config = NameServerConfig::new(local_addr, Protocol::Udp);
+    config.add_name_server(nameserver_config);
+
+    let resolver = AsyncResolver::tokio(config, ResolverOpts::default());
+    let result = resolver.lookup_ip("www.example.com.").await;
+
+    match result.unwrap_err().kind() {
+        ResolveErrorKind::NoRecordsFound { response_code, .. } => {
+            assert_eq!(*response_code, ResponseCode::NXDomain)
+        }
+        _ => panic!("wrong response code"),
+    };
 
     Ok(())
 }
